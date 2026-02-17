0e9be2062f79ab336f95747d1c586eb2e457b745
Yuvrajsinh05 <yuvrajsinh73598@gmail.com>
2026-02-17 16:46:17 +0530

    Handle all the Routes

--- src/app.ts
+++ src/app.ts
@@ -6,12 +6,27 @@ import { errorHandler } from "./middlewares/error.middleware.js";
 import { swaggerSpec } from "./docs/swagger.js";
 
 const app = express();
+app.use(express.json());
+
+// Health route
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "🚀 Broker Connect API is running",
+  });
+});
 
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-app.use(express.json());
+
 
 app.use("/api/v1", v1Routes);
 
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: `Route ${req.originalUrl} not found`,
+  });
+});
 app.use(errorHandler);
 
 
